eb7def3fb8cc38b4757c48a34987aa1196359e1f
95028555@qq.com <yangyi199159>
2019-10-12 09:19:17 +0800

    add close

--- engine.go
+++ engine.go
@@ -5,15 +5,27 @@ import (
 )
 
 type Engine struct {
-	engine *sql.DB
-	s      string
-	err    error
+	db  *sql.DB
+	s   string
+	err error
 }
 
 func Connect(dns string) (d Engine, err error) {
 	d = Engine{}
 	open, err := sql.Open("mysql", dns)
-	d.engine = open //连接数据库
+	d.db = open //连接数据库
 	d.err = err
 	return
+}
+
+func (e Engine) Ping() error {
+	return e.db.Ping()
+}
+
+func (e *Engine) Close() error {
+	return e.db.Close()
+}
+
+func (e *Engine) Query(query string, args ...interface{}) (*sql.Rows, error) {
+	return e.db.Query(query, args)
 }
